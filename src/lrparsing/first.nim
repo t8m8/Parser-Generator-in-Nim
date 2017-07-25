b1ee6e8b6d76c0f6c8ae52089e9edd39ff223802
t8m8 <t0m09488@gmail.com>
2017-07-25 22:20:52 +0900

    Update first.nim

--- src/lrparsing/first.nim
+++ src/lrparsing/first.nim
@@ -3,17 +3,20 @@ import tables, sequtils
 
 var firstSet = newTable[Token, seq[Token]]()
 
-proc first*(nt: Token, syntax: Syntax): seq[Token] =
-  if firstSet.hasKey(nt):
-    return firstSet[nt]
-  firstSet.add(nt, @[])
+proc first*(t: Token, syntax: Syntax): seq[Token] =
+  if firstSet.hasKey(t):
+    return firstSet[t]
+  elif t.isTerminal:
+    firstSet.add(t, @[t])
+    return @[t]
+  firstSet.add(t, @[])
   result = newSeq[Token]()
   for rule in syntax:
-    if rule.left != nt: continue
+    if rule.left != t: continue
     var token = rule.right[0]
     if token.isTerminal:
       result.add(token)
     else:
       result.insert(first(token, syntax))
   result = result.deduplicate()
-  firstSet.add(nt, result)
+  firstSet.add(t, result)
